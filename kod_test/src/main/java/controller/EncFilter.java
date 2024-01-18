d97d107caf561e864aef90d211664d170ba20e5c
김진영 <129861996+publicKJY94@users.noreply.github.com>
2024-01-18 10:27:01 +0900

    Update EncFilter.java

--- kod_test/src/main/java/controller/EncFilter.java
+++ kod_test/src/main/java/controller/EncFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpFilter;
 // 중괄호는 매핑이 여러개 발생했기 때문이다
 // 컨테이너의 역할 >> 객체를 프로그램에서 생성(new) 및 관리(싱글톤 패턴) 수행(메서드 사용가능)[사용자가 생성 x]
 public class EncFilter extends HttpFilter implements Filter {
-	private String endocing;
+	private String encoding;
 
 	public EncFilter() {
 		super();
@@ -39,7 +39,7 @@ public class EncFilter extends HttpFilter implements Filter {
 	// 자바는 멤버변수를 생성자에서 초기화 하지만, filter는 그 행동을 인식하고 있기 때문에 init에서 실행 >> 유사 생성자
 	// 최초에 단 한번만 실행되는 메서드
 	public void init(FilterConfig fConfig) throws ServletException {
-		encoding = "UTF-8";
+		this.encoding=fConfig.getServletContext().getInitParameter("encoding");
 	}
 
 }
