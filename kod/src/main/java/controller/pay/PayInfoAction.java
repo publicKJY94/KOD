3efddde80b78236eb8f29c6349a9b7b5c9b4af10
ParkHyeonmin <824hm@naver.com>
2024-02-12 16:01:29 +0900

    2024-02-12 / 박현민 / 비로그인 상태 구매 막기, 결제 취소, 사용하지 않는 파일 삭제

--- kod/src/main/java/controller/pay/PayInfoAction.java
+++ kod/src/main/java/controller/pay/PayInfoAction.java
@@ -22,44 +22,50 @@ public class PayInfoAction implements Action{
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ActionForward forward = new ActionForward();
-		forward.setPath("payInfo.jsp");
-		forward.setRedirect(false);
-		
 		
 		CartDAO cartDAO = new CartDAO();
 		//cartDTO.setCartProductCnt(Integer.parseInt(request.getParameter("purchaseCnt")));
 		ArrayList<CartDTO> datas = new ArrayList<CartDTO>();
 		HttpSession session =request.getSession();	
-		String memberID = ((MemberDTO)session.getAttribute("memberDTO")).getMemberID();
 		
-		String[] selectedProducts = request.getParameterValues("selectedProducts");
-		
-		if (selectedProducts != null) {
-		    for (String product : selectedProducts) {
-		        System.out.println("체크박스 값: " + product);
-		        CartDTO cartDTO = new CartDTO();
-		        cartDTO.setMemberID(memberID);
-		        cartDTO.setProductID(Integer.parseInt(product));
-		        cartDTO = cartDAO.selectOne(cartDTO);
-		        System.out.println(cartDTO);
-		        datas.add(cartDTO);
-		        System.out.println("선택된 장바구니 정보 : "+ datas);
-		    }
-		} else {
-		    System.out.println("선택된 체크박스가 없습니다."); // 추후 alert창 띄우기
+		if(session.getAttribute("memberDTO")== null) { // 로그인을 하지 않은 경우 로그인 페이지로 이동
+			forward.setPath("loginPage.do");
+			forward.setRedirect(true);
 		}
-		
-		request.setAttribute("cartDTO", datas);
-		System.out.println(datas);
-		
-		AddressDTO aDTO = new AddressDTO();
-		AddressDAO aDAO = new AddressDAO();
-		
-		aDTO.setMemberID(memberID);
-		aDTO.setSearchCondition("장바구니배송지");
-		aDTO = aDAO.selectOne(aDTO);
-		System.out.println("결제전 주소지 : "+aDTO);
-		request.setAttribute("addressDTO", aDTO);
+		else { // 로그인했을 경우 결제 전 페이지로 이동
+			String memberID = ((MemberDTO)session.getAttribute("memberDTO")).getMemberID();
+			String[] selectedProducts = request.getParameterValues("selectedProducts");
+			
+			if (selectedProducts != null) {
+			    for (String product : selectedProducts) {
+			        System.out.println("체크박스 값: " + product);
+			        CartDTO cartDTO = new CartDTO();
+			        cartDTO.setMemberID(memberID);
+			        cartDTO.setProductID(Integer.parseInt(product));
+			        cartDTO = cartDAO.selectOne(cartDTO);
+			        System.out.println(cartDTO);
+			        datas.add(cartDTO);
+			        System.out.println("선택된 장바구니 정보 : "+ datas);
+			    }
+			} else {
+			    System.out.println("선택된 체크박스가 없습니다."); // 추후 alert창 띄우기
+			}
+			
+			request.setAttribute("cartDTO", datas);
+			System.out.println(datas);
+			
+			AddressDTO aDTO = new AddressDTO();
+			AddressDAO aDAO = new AddressDAO();
+			
+			aDTO.setMemberID(memberID);
+			aDTO.setSearchCondition("장바구니배송지");
+			aDTO = aDAO.selectOne(aDTO);
+			System.out.println("결제전 주소지 : "+aDTO);
+			request.setAttribute("addressDTO", aDTO);
+			
+			forward.setPath("payInfo.jsp");
+			forward.setRedirect(false);
+		}
 		
 		return forward;
 	}
