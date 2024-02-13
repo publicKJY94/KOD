88e41022928f89a408505ccd152aebd775776dda
ParkHyeonmin <824hm@naver.com>
2024-02-13 17:20:29 +0900

    2024-02-13 / 박현민 / 결제 주석 및 로그 정리

--- kod/src/main/java/controller/pay/PayInfoAction.java
+++ kod/src/main/java/controller/pay/PayInfoAction.java
@@ -34,36 +34,36 @@ public class PayInfoAction implements Action{
 		}
 		else { // 로그인했을 경우 결제 전 페이지로 이동
 			String memberID = ((MemberDTO)session.getAttribute("memberDTO")).getMemberID();
-			String[] selectedProducts = request.getParameterValues("selectedProducts");
+			String[] selectedProducts = request.getParameterValues("selectedProducts");		// 장바구니에서 선택한 상품번호 받아오기
 			
-			if (selectedProducts != null) {
+			if (selectedProducts != null) {	
 			    for (String product : selectedProducts) {
-			        System.out.println("체크박스 값: " + product);
+			        System.out.println("장바구니에서 선택한 상품번호 : " + product);
 			        CartDTO cartDTO = new CartDTO();
 			        cartDTO.setMemberID(memberID);
 			        cartDTO.setProductID(Integer.parseInt(product));
-			        cartDTO = cartDAO.selectOne(cartDTO);
-			        System.out.println(cartDTO);
-			        datas.add(cartDTO);
+			        cartDTO = cartDAO.selectOne(cartDTO);	// 장바구니에서 선택한 상품의 정보만 받아오기
+			        //System.out.println(cartDTO);
+			        datas.add(cartDTO);						// 선택한 상품의 정보를 리스트에 저장
 			        System.out.println("선택된 장바구니 정보 : "+ datas);
 			    }
 			} else {
-			    System.out.println("선택된 체크박스가 없습니다."); // 추후 alert창 띄우기
+			    System.out.println("선택된 체크박스가 없습니다."); // 선택한 상품이 없는 경우
 			}
 			
-			request.setAttribute("cartDTO", datas);
-			System.out.println(datas);
+			request.setAttribute("cartDTO", datas);		// 'cartDTO'라는 변수에 선택된 상품의 리스트 정보를 저장
+			//System.out.println("cartDTO 변수에 담긴 상품 리스트 : "+datas);
 			
 			AddressDTO aDTO = new AddressDTO();
 			AddressDAO aDAO = new AddressDAO();
 			
 			aDTO.setMemberID(memberID);
 			aDTO.setSearchCondition("장바구니배송지");
-			aDTO = aDAO.selectOne(aDTO);
-			System.out.println("결제전 주소지 : "+aDTO);
-			request.setAttribute("addressDTO", aDTO);
+			aDTO = aDAO.selectOne(aDTO); 				// 해당 사용자의 가장 최근에 생성된 주소지를 가져옴
+			System.out.println("결제 전 주소지 정보확인 : "+aDTO);
+			request.setAttribute("addressDTO", aDTO);	// 'addressDTO'라는 변수에 해당 사용자 주소지 정보를 저장
 			
-			forward.setPath("payInfo.jsp");
+			forward.setPath("payInfo.jsp");				// payInfo.jsp로 이동
 			forward.setRedirect(false);
 		}
 		
